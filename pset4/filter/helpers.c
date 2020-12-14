317ed32aa3f866263abf1f378e88859c5c52c5d8
Gerald Southwood <gerald@vertx.co.za>
2020-12-14 20:29:06 +0200

    Filter - Blur complete
    Signed-off-by: Gerald Southwood <gerald@vertx.co.za>

--- pset4/filter/helpers.c
+++ pset4/filter/helpers.c
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdio.h>
 
-// cap an integer between a min and max
+// cap an integer between a min and max number
 int capInt(int num, int min, int max)
 {
     if (num < min)
@@ -19,8 +19,6 @@ int capInt(int num, int min, int max)
     }
 }
 
-//int average()
-
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -30,16 +28,18 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         // iterate through rows
         for (int j = 0; j < height; j++)
         {
+            // get RGB values
             float red = image[j][i].rgbtRed;
             float green = image[j][i].rgbtGreen;
             float blue = image[j][i].rgbtBlue;
+            
             // calculate the average for the red, green, blue properties of each pixel
             float avg = round((red + green + blue) / 3);
 
             // assign the average back to each of those properties
-            image[j][i].rgbtRed = (int)avg;
-            image[j][i].rgbtGreen = (int)avg;
-            image[j][i].rgbtBlue = (int)avg;
+            image[j][i].rgbtRed = round(avg);
+            image[j][i].rgbtGreen = round(avg);
+            image[j][i].rgbtBlue = round(avg);
         }
     }
     return;
@@ -54,7 +54,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
         // iterate through rows
         for (int j = 0; j < height; j++)
         {
-            // get the value for r, g, b for each pixel
+            // get rgb values
             float originalRed = image[j][i].rgbtRed;
             float originalGreen = image[j][i].rgbtGreen;
             float originalBlue = image[j][i].rgbtBlue;
@@ -97,67 +97,63 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // create a copy of the image
-    // Iterate through each pixel of the original image
-    // For each pixel, calculate its new RGBTRIPLE values based on the values of the values of the copied image
-    // when complete, delete the copied image
-
-    //RGBTRIPLE c = malloc(sizeof(RGBTRIPLE));
-
-    int h = height;
-    int w = width;
-
+    // COPY IMAGE
+    // Create placeholder of type 'image'
     RGBTRIPLE temp_copy[height][width];
+    // iterate through columns
     for (int i = 0; i < width; i++)
     {
+        // iterate through rows
         for (int j = 0; j < height; j++)
         {
+            // copy pixel rgb from original image
             temp_copy[j][i].rgbtRed = image[j][i].rgbtRed;
             temp_copy[j][i].rgbtGreen = image[j][i].rgbtGreen;
             temp_copy[j][i].rgbtBlue = image[j][i].rgbtBlue;
         }
     }
 
+    // Calculate new value of each pixel based on values of surrounding pixels
 
+    // Iterate through columns
     for (int i = 0; i < width; i++)
     {
+        // Iterate through rows
         for (int j = 0; j < height; j++)
         {
-            // take average of 9 surrounding pixels
-            // rework edges, currently it is counting the pixels along edges multiple times
-            // by normalising out of bound pixels to 0 or max
-            // fix by disregarding these pixels rather than double counting them
-            // maybe use if functions
-            image[j][i].rgbtRed = (temp_copy[capInt(j-1, 0, h-1)][capInt(i-1, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j-1, 0, h-1)][capInt(i, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j-1, 0, h-1)][capInt(i+1, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i-1, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i+1, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i-1, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i, 0, w-1)].rgbtRed
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i+1, 0, w-1)].rgbtRed)
-                                    / 9;
-            image[j][i].rgbtGreen = (temp_copy[capInt(j-1, 0, h-1)][capInt(i-1, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j-1, 0, h-1)][capInt(i, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j-1, 0, h-1)][capInt(i+1, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i-1, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i+1, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i-1, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i, 0, w-1)].rgbtGreen
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i+1, 0, w-1)].rgbtGreen)
-                                    / 9;
-            image[j][i].rgbtBlue = (temp_copy[capInt(j-1, 0, h-1)][capInt(i-1, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j-1, 0, h-1)][capInt(i, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j-1, 0, h-1)][capInt(i+1, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i-1, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j, 0, h-1)][capInt(i+1, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i-1, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i, 0, w-1)].rgbtBlue
-                                    + temp_copy[capInt(j+1, 0, h-1)][capInt(i+1, 0, w-1)].rgbtBlue)
-                                    / 9;
+            // Declare variables
+            int counter = 0;
+            float total_red = 0;
+            float total_green = 0;
+            float total_blue = 0;
+            int blur_level = 1;
+            
+            // iterate through surrounding pixel columns
+            for (int k = (i - blur_level); k <= (i + blur_level); k++)
+            {
+                // check position is not out of bounds
+                if (k >= 0 && k < width)
+                {
+                    // iterate through surrounding pixel columns
+                    for (int l = (j - blur_level); l <= (j + blur_level); l++)
+                    {
+                        // check position is not out of bounds
+                        if (l >= 0 && l < height)
+                        {
+                            // total rgb values for average calculations
+                            total_red += temp_copy[l][k].rgbtRed;
+                            total_green += temp_copy[l][k].rgbtGreen;
+                            total_blue += temp_copy[l][k].rgbtBlue;
+                            counter++;
+                        }
+                    }
+                }
+            }
+            
+            // set new rgb values of pixel
+            image[j][i].rgbtRed = round(total_red / counter);
+            image[j][i].rgbtGreen = round(total_green / counter);
+            image[j][i].rgbtBlue = round(total_blue / counter); 
         }
     }
 
