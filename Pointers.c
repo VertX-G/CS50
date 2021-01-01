3bbee0b6295c7f719da73b47c1a1f0c3eb0d73e5
Gerald Southwood <72921662+VertX-G@users.noreply.github.com>
2021-01-01 09:17:49 +0200

    Create Pointers.c

---
+++ Pointers.c
@@ -0,0 +1,13 @@
+/*
+Pointers are data types themselves
+A pointer is a data item whose:
+• Value is a memory address
+• Type describes the data located at that memory address
+
+
+*x returns the data that lives at the memory address stored in the pointer x
+
+&x extracts the value of the address of variable "x"
+&arr[i] extracts the value of the address of arr[i]
+
+/*
