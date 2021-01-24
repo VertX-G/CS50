444cf5da38d97fdaa3b9f1c61ba7b0e05897fb01
Gerald Southwood <gerald@vertx.co.za>
2021-01-24 21:21:12 +0200

    pset6/readability - completed pset6/dna - started

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -1,20 +1,29 @@
+import re
 from cs50 import get_string
 
 
-# ord(c) - input a unicode character as a string, and returns it's integer
-# chr(i) - input unicode integer, and returns string character
+# get_string for user input
+text = get_string("Text: ")
+
+letters = "[a-zA-Z]"    # any a to z or A to Z characters
+letterCount = len(re.findall(letters, text))
+words = "\S+"   # any collection of 1 or more non-whitespace characters
+wordCount = len(re.findall(words, text))
+sentences = "[\.\!\?]"  # any occurence of a ".", "!", or "?" marks the end of a sentence
+sentenceCount = len(re.findall(sentences, text))
+
+L = (100/wordCount)*letterCount     # L = average number of letters per 100 words
+S = (100/wordCount)*sentenceCount   # S = average number of sentences per 100 words
 
 # Coleman-Liau index = (0.0588 * L - 0.296 * S - 15.8)
-# L = average number of letters per 100 words
-# S = average number of sentences per 100 words
-
-# get_string for user input
-
-# count number of letters, words and sentences in the text
-# letter =  any a to z or A to Z
-# words = any sequence of characters separated by spaces
-# sentence = any occurence of a ".", "!", or "?" marks the end of a sentence
+f = round(0.0588 * L - 0.296 * S - 15.8)
 
 # program should print "Grade X" where X is the grade level computed by Coleman_Liau formula, rounded to nearest integer.
 # if index number is >=16, output "Grade 16+"
 # is index number is <1, output "Before Grade 1"
+if f < 1:
+    print(f"Before Grade 1")
+elif f >= 16:
+    print(f"Grade 16+")
+else:
+    print(f"Grade {f}")
